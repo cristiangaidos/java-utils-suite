2f219b5b8d065bef0def140d63b2bc3aead9af37
cristiangaidos <gaidoscristi@gmail.com>
2024-06-11 18:53:00 +0200

    Update copyNode.js

--- copyNode.js
+++ copyNode.js
@@ -1,55 +1,59 @@
 async function copySelectedNodes(nodeList, allowedPasting) {
+    if (nodeList && nodeList.length > 0 && !allowedPasting) {
+        for (var originData of nodeList) {
+            // Copy the node data (excluding the ID, which should be unique)
+            var nodeData = Object.assign({}, originData);
+            delete nodeData.id; // Ensure the new node gets a unique ID
 
-  if(nodeList && nodeList.length > 0 && !allowedPasting) {
+            // Retrieve and modify the position
+            var originalPosition = jsRenderer.getCoordinates(originData.id);
+            var newPosition = {
+                x: originalPosition.x + 15, // Shift 15 pixels to the right
+                y: originalPosition.y + 15  // Shift 15 pixels down
+            };
 
-  for( var originData of nodeList ) {
-         // Copy the node data (excluding the ID, which should be unique)
-         var nodeData = Object.assign({}, originData);
-         delete nodeData.id; // Ensure the new node gets a unique ID
-     
-     
-         // Retrieve and modify the position
-         var originalPosition = jsRenderer.getCoordinates(originData.id);
-         var newPosition = {
-             x: originalPosition.x + 15, // Shift 10 pixels to the right
-             y: originalPosition.y + 15  // Shift 10 pixels down
-         };
-           
-         if (nodeData.type === "tariff") {
-           await copyTariffNodeWithCallback(  { name: "nodeId", value: originData.id }, function(nodeId:string,text:string) {
-             nodeData.id = nodeId;
-             nodeData.text = text;
-             nodeData.left = newPosition.x;
-             nodeData.top = newPosition.y;
-             let newNode = jsToolkit.addNode(nodeData);
-             jsRenderer.setPosition(newNode, newPosition.x, newPosition.y);       
-           });    
-         } else if (nodeData.type === "cairTariff") {
-           await copyTariffNodeWithCallback({ name: "nodeId", value: originData.id }, function(nodeId:string,text:string, ruleNumber:string) {
-             nodeData.id = nodeId;
-             nodeData.text = text;
-             nodeData.ruleNumber = ruleNumber;
-             nodeData.left = newPosition.x;
-             nodeData.top = newPosition.y;
-             let newNode = jsToolkit.addNode(nodeData);     
-             jsRenderer.setPosition(newNode, newPosition.x, newPosition.y); 
-           });
-          } else if (nodeData.type === "pricingProduct") {
-     /*       copyProductNodeWithCallback({ name: "nodeId", value: originData.id }, function(nodeId:string,text:string) {
-             nodeData.id = nodeId;
-             nodeData.text = text;
-             nodeData.left = newPosition.x;
-             nodeData.top = newPosition.y;
-             let newNode = jsToolkit.addNode(nodeData);
-             jsRenderer.setPosition(newNode, newPosition.x, newPosition.y);   
-           }); */    
-         } else {
-           nodeData.id = uuid();
-           nodeData.left = newPosition.x;
-           nodeData.top = newPosition.y;
-           let newNode = jsToolkit.addNode(nodeData);
-           jsRenderer.setPosition(newNode, newPosition.x, newPosition.y);     
-         }
-  }  
-  }
+            if (nodeData.type === "tariff") {
+                const result = await promisifyCopyTariffNode({ name: "nodeId", value: originData.id });
+                nodeData.id = result.nodeId;
+                nodeData.text = result.text;
+                nodeData.left = newPosition.x;
+                nodeData.top = newPosition.y;
+                let newNode = jsToolkit.addNode(nodeData);
+                jsRenderer.setPosition(newNode, newPosition.x, newPosition.y);
+
+            } else if (nodeData.type === "cairTariff") {
+                const result = await promisifyCopyTariffNode({ name: "nodeId", value: originData.id });
+                nodeData.id = result.nodeId;
+                nodeData.text = result.text;
+                nodeData.ruleNumber = result.ruleNumber;
+                nodeData.left = newPosition.x;
+                nodeData.top = newPosition.y;
+                let newNode = jsToolkit.addNode(nodeData);
+                jsRenderer.setPosition(newNode, newPosition.x, newPosition.y);
+
+            } else if (nodeData.type === "pricingProduct") {
+                // Handle pricingProduct similarly if needed
+                // ...
+
+            } else {
+                nodeData.id = uuid();
+                nodeData.left = newPosition.x;
+                nodeData.top = newPosition.y;
+                let newNode = jsToolkit.addNode(nodeData);
+                jsRenderer.setPosition(newNode, newPosition.x, newPosition.y);
+            }
+        }
+    }
+}
+
+function promisifyCopyTariffNode(data) {
+    return new Promise((resolve, reject) => {
+        copyTariffNodeWithCallback(data, (nodeId, text, ruleNumber) => {
+            if (ruleNumber !== undefined) {
+                resolve({ nodeId, text, ruleNumber });
+            } else {
+                resolve({ nodeId, text });
+            }
+        });
+    });
 }
